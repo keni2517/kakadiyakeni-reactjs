9c5ff6b660a70278c2cb781563cb255c8254961b
keni <kenikakadiya@gmail.com>
2024-01-19 07:50:53 +0530

    java

---
+++ Javascript/lecture2.js
@@ -0,0 +1,34 @@
+var z = 20;
+{
+    var x = 20;
+}
+{
+    var y = 40;
+}
+
+
+console.log(y)
+console.log(x);
+
+{
+    let x = "hello"
+    x = 'javascript'
+    console.log(x);
+}
+
+{ let x = 'sun' }
+
+{
+    let string1  = "He'llo"
+    let string2  = 'He"llo'
+    let string3  = "He\"llo"
+    let string4  = 'He\'llo'
+    let string5  = "He\nllo"
+    let string6  = "He\t\tllo"
+    console.log(string1);
+    console.log(string2);
+    console.log(string3);
+    console.log(string4);
+    console.log(string5);
+    console.log(string6);
+}
