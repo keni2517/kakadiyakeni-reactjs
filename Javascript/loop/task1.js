dcde9f2bd5cd545126ed64f8341cf16e959c5a1a
keni <kenikakadiya@gmail.com>
2024-01-23 18:41:11 +0530

    javascript

--- Javascript/loop/task1.js
+++ Javascript/loop/task1.js
@@ -1,29 +1,11 @@
-// For Loop 
+// CountDown Using while loop
 
 
 {
-    let i;
-    for(i=10 ; i<=20 ; i++){
+    let i = 10;
+
+    while (i > 0) {
         console.log(i);
+        i--;
     }
-}
-
-// While Loop
-
-{
-    let i=1;
-    while (i<=10){
-        document.write(i+"<br>");
-        i++;
-    }
-}
-
-//Do While Loop
-
-{
-    let i=10;
-    do{
-        console.log(i);
-        i++;
-    }while (i<=20);
 }
