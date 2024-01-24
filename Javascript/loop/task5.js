c198b3b418dcf758bc91d0d6c63381a7c2555288
keni <kenikakadiya@gmail.com>
2024-01-24 08:14:15 +0530

    javascript

--- Javascript/loop/task5.js
+++ Javascript/loop/task5.js
@@ -1,1 +1,18 @@
 // Generate the Fibonacci sequence using a while loop
+
+{
+    let i = 1;
+    let a = 0;
+    let n = 5;
+    let b = 1;
+    let c;
+    console.log("Fibonacci sequence:");
+    while (i<=n)
+    {
+        c = a+b;
+        a = b;
+        b = c;
+        console.log(a);
+        i++;
+    }
+}
