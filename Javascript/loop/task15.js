cd2b2decab2b979cf9142c0480d34cf3a9cceeb1
keni <kenikakadiya@gmail.com>
2024-01-25 08:09:01 +0530

    javascript

--- Javascript/loop/task15.js
+++ Javascript/loop/task15.js
@@ -1,1 +1,19 @@
 // Skip multiples of 3 using a while loop and continue statement
+
+{
+    let number = 1;
+
+    while (number <= 20) {
+
+        if (number % 3 === 0) {
+            number++;
+            continue;
+        }
+
+
+        console.log(number);
+
+
+        number++;
+    }
+}
