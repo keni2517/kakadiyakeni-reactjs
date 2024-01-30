2d29254671356f389220f81db75f0ae6aa12a089
keni <kenikakadiya@gmail.com>
2024-01-30 08:11:02 +0530

    javascript

---
+++ Javascript/lecture4.js
@@ -0,0 +1,20 @@
+{
+    let x;
+    console.log(x);  /*"undefined"*/
+    console.log(typeof (x));
+}
+
+{
+    //const x;
+}
+
+{
+    console.log(x); /* ReferenceError */
+    const x = 3;
+}
+
+{
+    console.log(y); /* ReferenceError */
+    const y = 3;
+}
+
--- Javascript/loop/task7.js
+++ Javascript/loop/task7.js
@@ -1,4 +1,20 @@
 // Check if a word is a palindrome using a while loop
 
 
-  
+{
+    let i , n=121 , reversedNumber = 0;
+    let j = n;
+    while(n != 0) 
+    {
+        i = n % 10;
+        reversedNumber = reversedNumber * 10 + i ;
+        n /= 10;
+    }
+    if(n == reversedNumber)
+        console.log(reversedNumber);
+    else
+    {
+        console.log("not a number");
+    }
+}
+
