b5cc126d8c4853724d9e1f068b93585a4344dd09
keni <kenikakadiya@gmail.com>
2024-01-31 08:07:18 +0530

    javascript

--- Javascript/loop/task12.js
+++ Javascript/loop/task12.js
@@ -1,2 +1,13 @@
 // Check if a number is prime using a for loop and break statement
 
+{
+    let i,n = 5;
+    for (i = 1; i <= 10; i++) {
+
+        if (n%i == 0) {
+            break;
+        }
+
+        console.log(i);
+    }
+}
