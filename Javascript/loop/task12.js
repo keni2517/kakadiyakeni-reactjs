d5b2bef3ef4c7b01d67b2a671c1de544e2a9a446
keni <kenikakadiya@gmail.com>
2024-01-31 08:42:18 +0530

    javascript

--- Javascript/loop/task12.js
+++ Javascript/loop/task12.js
@@ -1,8 +1,8 @@
 // Check if a number is prime using a for loop and break statement
 
 {
-    let i,n = 15;
-    for (i = 2; i <= n; i++) {
+    let i,n = 11;
+    for (i = 2; i < n; i++) {
 
         if (n%i == 0) {
             console.log(`${i} number: ${n} is not prime`);
