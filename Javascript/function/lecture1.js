8b438ac848d5305ed336b4ac1a71d62d81e3b349
keni <kenikakadiya@gmail.com>
2024-02-02 08:52:59 +0530

    javascript

---
+++ Javascript/function/lecture1.js
@@ -0,0 +1,75 @@
+//Math.E function
+
+let math1 = Math.E
+console.log(math1); //2.718281828459045
+
+//Math.PI function
+
+let math2 = Math.PI
+console.log(math2); //3.1415
+
+//Math round()
+
+let math3 = Math.round(22.9) //23
+let math4 = Math.round(22.3) //22
+let math5 = Math.round(-22.9) //-23
+let math6 = Math.round(-22.3) //-22
+console.log(math3);
+console.log(math4);
+console.log(math5);
+console.log(math6);
+
+//Math.ceil()
+
+let math7 = Math.ceil(89.1)
+let math8 = Math.ceil(-89.1)
+console.log(math7);
+console.log(math8);
+
+//Math.floor()
+
+let math9 = Math.floor(85.7)
+let math10 = Math.floor(-85.1)
+console.log(math9);
+console.log(math10);
+
+//Math.trunc()
+
+let math11 = Math.trunc(45.896325)
+console.log(math11);
+
+//Math.sign()
+
+let math12 = Math.sign(0)
+let math13 = Math.sign(-10)
+let math14 = Math.sign(10)
+console.log(math12);
+console.log(math13);
+console.log(math14);
+
+//Math.pow()
+
+let math15 = Math.pow(5 , -8)
+console.log(math15);
+
+//Math.sqrt()
+
+let math16 = Math.sqrt(25);
+console.log(math16);
+
+//Math.abs()
+
+let math17 = Math.abs(-9)
+console.log(math17);
+
+//Math.min() and Math.max()
+
+let math18  = Math.min(-15  , 155 , 20  , 50  , 230)
+let math19  = Math.max(-15  , -155 , 20  , 50  , 530)
+console.log(math18);
+console.log(math19);
+
+//Math.random()
+
+const math20 = Math.random(5)*10
+console.log(math20);
